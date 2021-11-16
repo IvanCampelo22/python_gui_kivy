242b1fd659bb6e038c02bd2b32f00b39daef6091
IvanCampelo22 <ivanilsondasilva1973@gmail.com>
2021-11-16 00:44:12 -0300

    subindo todos os arquivos sobre os estudos de db_api

---
+++ db-api/cliente_repositorio.py
@@ -0,0 +1,43 @@
+import MySQLdb
+import fabricar_conexao
+
+
+class ClienteRepositorio():
+
+    @staticmethod
+    def listar_clientes():
+        fabrica = fabricar_conexao.FabricaConexao.conectar()
+        try:
+            cursor = fabrica.cursor()
+            cursor.execute("select * from cliente")
+            print(cursor.fetchall())
+        finally:
+            fabrica.close()
+
+    @staticmethod
+    def inserir_clientes(cliente):
+        fabrica = fabricar_conexao.FabricaConexao.conectar()
+        try:
+            cursor = fabrica.cursor()
+            cursor.execute("insert into cliente (nome, idade) values (%s, %s)", (cliente.nome, cliente.idade))
+        finally:
+            fabrica.close()
+
+    @staticmethod
+    def editar_cliente(id_cliente, cliente):
+        fabrica = fabricar_conexao.FabricaConexao.conectar()
+        try:
+            cursor = fabrica.cursor()
+            cursor.execute("update cliente set nome=%(nome)s, idade=%(idade)s where idcliente=%(id_cliente)s",
+                       ({'nome': cliente.nome, 'idade': cliente.idade, 'id_cliente': id_cliente}))
+        finally:
+            fabrica.close()
+
+    @staticmethod
+    def remover_cliente(id_cliente):
+        fabrica = fabricar_conexao.FabricaConexao.conectar()
+        try:
+            cursor = fabrica.cursor()
+            cursor.execute("delete from cliente where idcliente=%s", (id_cliente,))
+        finally:
+            fabrica.close()
---
+++ db-api/main.py
@@ -0,0 +1,9 @@
+import cliente
+import cliente_repositorio
+
+clientes = cliente.Cliente("João", 29)
+
+cliente_repositorio.ClienteRepositorio.listar_clientes()
+cliente_repositorio.ClienteRepositorio.inserir_clientes(clientes)
+#.ClienteRepositorio.editar_cliente(1, clientes)
+#cliente_repositorio.ClienteRepositorio.remover_cliente(25)
