72c015a49f732602c928a33209f1d7da4ee3b198
IvanCampelo22 <ivanilsondasilva1973@gmail.com>
2021-11-19 08:20:21 -0300

    exemplos de interfaces

---
+++ python-kivy/gui/layouts/box.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+
+
+class TelaApp(BoxLayout):
+    pass
+
+
+class Box(App):
+    def build(self):
+        return TelaApp()
+
+
+Box().run()
---
+++ python-kivy/gui/layouts/grid.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+
+
+class TelaApp(GridLayout):
+    pass
+
+
+class Grid(App):
+    def build(self):
+        return TelaApp()
+
+
+Grid().run()
